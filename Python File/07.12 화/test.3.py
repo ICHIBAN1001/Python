f64403496e2f593b0aee4ff4d267a8c346d8fb1f
ICHIBAN1001 <108773192+ICHIBAN1001@users.noreply.github.com>
2022-07-13 20:21:51 +0900

    Update test.3.py

--- Python File/07.12 화/test.3.py
+++ Python File/07.12 화/test.3.py
@@ -13,26 +13,57 @@
 #   – 컴퓨터에서 생성 한 난수 값을 자리 순서대로 맞출 경우
 #------------------------------------------------------------------------------------------------------------------
 
-
 # import random 으로 random 함수 부르기
 import random
 
+# random 함수를 담을 리스트 변수 작성 (2개 이상)
+ransulist = []
+# 반복문으로 범위 지정 개수 만큼 생성
+while len(ransulist) < 3 :
+    betty = int(random.random()*10)
+    if betty not in ransulist :
+        ransulist.append(betty)
+
+
 # 시도횟수 변수
 count = 1
-#출력기
-print("시도횟수 : ",count)
-print("정수 3개를 입력 하세요")
+# while 반복문 스위치 
+flag = True 
+# while 전체 반복 흐름 제어 
+while flag :
 
+    #출력기
+    print("시도횟수 : ",count)
+    print("정수 3개를 입력 하세요")
 
-# random 함수를 담을 리스트 변수 작성 (2개 이상)
-mylist = []
-# 반복문으로 범위 지정 개수 만큼 생성
-while len(mylist) < 3 :
-    betty = int(random.random()*10)
-    if betty not in mylist :
-        mylist.append(betty)
+    # 3개의 정수를 입력 받기 
+    mylist = []
+    for value in range (len(ransulist)) :   # len(ransulist) = 3
+        mylist.append(int(input(str(value+1)+"번째 정수를 입력하세요 :")))
 
-# 3개의 정수를 입력 받기 
-jslist = []
-for value in range (len(mylist)) :   # len(mylist) = 3
-    jslist.append(int(input(str(value+1)+"번째 정수를 입력하세요 :")))
+    # Strike, Ball, Out 변수 공간
+    Strike = 0
+    Ball = 0
+    Out = 0
+    # 입력 받은 값들을 ransulist 의 요소 값들과 비교
+    for index in range (len(mylist)) :
+        element = mylist[index]
+    # Strike, Ball, Out 조건식 만들기 
+        # Strike / Ball
+        if element in ransulist :
+            # Strike
+            if element == ransulist[index] :
+                Strike += 1
+            # Ball    
+            else :
+                Ball += 1
+        # Out        
+        else :
+            Out += 1
+
+    # Strike, Ball 리셋 
+    Strike = 0
+    Ball = 0
+
+    
+                
