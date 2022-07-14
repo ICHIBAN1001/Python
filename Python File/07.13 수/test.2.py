607b88d7ec9e07b628a30152be9fec801a93f946
ICHIBAN1001 <108773192+ICHIBAN1001@users.noreply.github.com>
2022-07-14 12:16:12 +0900

    Delete test.2.py

--- Python File/07.13 수/test.2.py
+++
@@ -1,8 +0,0 @@
-def function (count) :
-    for value in range (count) :
-        orei = "先生、いつもありがとうございます。"
-        print(orei)
-        if len(orei)>= 10 :
-            print("今後とも何卒よろしくお願い申し上げます。") 
-    return print("용현아 주원아 사랑한다")
-function(5)
