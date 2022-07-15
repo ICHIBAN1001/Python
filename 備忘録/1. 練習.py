1eb3fc530b1e021dc156ca338c99fe25b8537faa
ICHIBAN1001 <108773192+ICHIBAN1001@users.noreply.github.com>
2022-07-15 18:27:19 +0900

    Update 1. 練習.py

--- 備忘録/1. 練習.py
+++ 備忘録/1. 練習.py
@@ -1,31 +1,51 @@
-# 1. 문구 출력 : 입력 문자열의 줄(Line) 수를 입력하세요! 
-LineNumber = int(input("입력 문자열의 줄(Line) 수를 입력하세요! : "))
-# 영문의 문자열 키보드로 입력 받아 리스트에 저장
-Word = ""           # 문자열로 계산
-WordList = []       # 리스트로 계산
-# 입력 받기  for 반복문 => 1의 수만큼 반복
-for Value in range (LineNumber) :
-    InputValue = input(str(Value+1) +" 번째 라인의 문자열을 입력하세요. : ")
-    Word += InputValue               # 문자열로 계산
-    WordList.append(InputValue)      # 리스트로 계산
+# # 1. 문구 출력 : 입력 문자열의 줄(Line) 수를 입력하세요! 
+# LineNumber = int(input("입력 문자열의 줄(Line) 수를 입력하세요! : "))
+
+# # 1.1. 영문의 문자열 키보드로 입력 받아 리스트에 저장
+# Word = ""           # "문자열"로  담을  변수
+# WordList = []       # "리스트"로  담을  변수
+
+# # 2. 입력 받기  for 반복문 => 1의 수만큼 반복
+# for Value in range (LineNumber) :
+#     InputValue = input(str(Value+1) +" 번째 라인의 문자열을 입력하세요. : ")
+#     Word += InputValue+" "               # 문자열로 계산
+#     WordList.append(InputValue)      # 리스트로 계산
     
-#검색할 문자열 확인  (인덱스로 확인)
+# # 3. 검색할 문자열 확인  (인덱스로 확인)
 Search = input("검색 할 문자열을 입력하세요. : ")
+Word = "hello world hello worldddd hello kkk"           # "문자열"로  담을  변수
+WordList = ['hello world', 'hello worldddd hello', 'kkk']       # "리스트"로  담을  변수
 print(Word)
-print(WordList)
-################################################  리스트 저장 
+print(WordList) 
+################################################  리스트 저장  ################################################ 
 
-linesu = ""
-# for 반복문으로 WordList 요소 하나씩 꺼내기 
-for index in range (len(WordList)) :
-    Element = WordList[index]  # ["qwwqw","qwwqw","qwwqw"]     Element = "qwwqw"
-    if Element in WordList :
-        linesu += (index+1)  
-print(linesu)
-# 검색된 라인수
-    
-# 검색된 횟수
+# index    0  1   2    3    4
+linesu = [ 1,",", 2 , "," , 3 ]    # 5개
 
-#총 단어 수
+linecount = 0
+line =""
+# 4. for 반복문으로 WordList 요소 하나씩 꺼내기 
+for element in WordList :  # index = [0] [1] [2]
+
+   
+
+# 5. 검색된 라인수
+# WordList 안에 Element 가 있으면 
+# print ("검색된 라인수" ,linecount)   
+
+                                                        
+ 
+# 6. 검색된 횟수
+# WordList 리스트 각 요소안에 Search가 있으면
+
+
+################################################
+# 7. 총 단어 수
+# WordList 안에 " " 에 +1
+blank = 0
+for value in Word :
+    if value == " " :
+        blank += 1
+print("총 단어 수 : ",blank)
 
     
