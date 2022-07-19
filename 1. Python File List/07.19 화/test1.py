59219bcbb918fc44bd6a648d871bffd76469547a
ICHIBAN1001 <108773192+ICHIBAN1001@users.noreply.github.com>
2022-07-19 17:42:42 +0900

    Update test1.py

--- 1. Python File List/07.19 화/test1.py
+++ 1. Python File List/07.19 화/test1.py
@@ -2,19 +2,26 @@ import random
 
 MyList = []
 
-duplicate = 0 
+newlist = []
+
 
 # while 로 같은거 안들어 오게 계속 무한 반복 
-while  len(MyList)< 25 :    # 0 ~24 개
+
+while len(MyList)< 25 :    # 0 ~24 개
+    
     Element = random.randint(1,50) 
     # 안에 안들어 있으면 넣기     
     if not Element in MyList :
         MyList.append(Element)
 
-print(MyList)  
-newlist = []
 for index in range (1,50+1) :
-    if index in MyList  :
+    if index in MyList :
         newlist.append(index)
       
 print(newlist)
+
+for value in range (len(newlist)) :
+    Element = newlist[value]
+    print(Element, "\t", end= "" )
+    if (value+1) %5 == 0 :
+        print()
