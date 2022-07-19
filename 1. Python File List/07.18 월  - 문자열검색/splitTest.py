139da8e846bbaeb5c5789bb68e70858d831c9e67
ICHIBAN1001 <108773192+ICHIBAN1001@users.noreply.github.com>
2022-07-19 10:33:21 +0900

    Update splitTest.py

--- 1. Python File List/07.18 월  - 문자열검색/splitTest.py
+++ 1. Python File List/07.18 월  - 문자열검색/splitTest.py
@@ -6,12 +6,14 @@ word = ""
 
 count = 0
 for value in msg:
+    # 단어 일 때
     if value.isalpha():
         word += value
-        
+        # 마지막 일 때
         if count == len(msg) - 1:
             list.append(word)
             word = ""
+    # 공백일 때
     else:
         if len(word) >= 1:
             list.append(word)
